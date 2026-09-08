6ddca27362b267099be67dcd97b03fd9806d71c5
Braden-Kapp <braden.h.kapp@gmail.com>
2026-09-08 00:58:33 -0500

    New logic for a fish catch minigame after a fish bites to start fishing

--- Scripts/LOADER.gd
+++ Scripts/LOADER.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var tracker = $CompletionTracker
 @onready var player = $Player
 @onready var FishAnimator = $FishAnimationPlayer
+@onready var UIAnimator = $UIAnimationPlayer
 
 #Sprites To Have a toggle all visibilty function here
 #Better Here than having to deal with all scripts individual
@@ -17,6 +18,10 @@ extends Node2D
 @onready var lineSprite = $Art/FishingLine
 @onready var CompletionBar = $CompletionTracker/ProgressBar
 
+@onready var UINotification = $Art/Notification
+var missed: bool = false
+var isCasting: bool = false
+
 func _ready():
 	if player.has_signal("cast"):
 		player.cast.connect(on_cast)
@@ -27,9 +32,12 @@ func _ready():
 	#fish.start_Fishing(manager.fish_library[0])
 	#tracker.start_tracking() 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Process handles the button check cleanly each frame without blocking the thread
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("fishButtons") and UIAnimator.is_playing() and UIAnimator.current_animation == "CatchUI":
+		UIAnimator.stop(false) # Stopping with false prevents trigger bugs
+		UINotification.visible = false
+		begin_fishin()
 
 func hide_fishing():
 	playerSprite.visible = false
@@ -37,6 +45,7 @@ func hide_fishing():
 	barSprite.visible = false
 	lineSprite.visible = false
 	CompletionBar.visible = false
+	UINotification.visible = false
 
 func show_fishing():
 	playerSprite.visible = true
@@ -48,11 +57,20 @@ func show_casting():
 	lineSprite.visible = true
 
 func on_cast():
+	if isCasting or player.fishing:
+		return
+	isCasting = true
+	missed = false
 	show_casting()
 	# Play fish animation and wait for finish
 	FishAnimator.play("FishBiteLower")
 	await FishAnimator.animation_finished
+	UINotification.visible = true
+	UIAnimator.play("CatchUI")
+
+func begin_fishin():
 	#Start Gameplay
+	isCasting = false
 	show_fishing()
 	player.fishing = true
 	fish.start_Fishing(manager.fish_library[0])
@@ -65,3 +83,19 @@ func on_catch():
 	FishAnimator.play("FishCaught")
 	await FishAnimator.animation_finished
 	player.fishing = false
+
+func missed_catch():
+	isCasting = false
+	missed = true
+	player.fishing = false
+	hide_fishing()
+	fish.stop_Fishing()
+	FishAnimator.play("RESET")
+
+func _on_ui_animation_player_animation_finished(anim_name: StringName) -> void:
+	# Only if the animation finished without catch
+	if not player.fishing:
+		missed = true
+		UINotification.visible = false
+		missed_catch()
+		
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,8 +5,8 @@ signal cast
 var fishing = false
 
 func _physics_process(_delta: float) -> void:
-	if !fishing && Input.is_action_pressed("fishButtons"):
+	if !fishing && Input.is_action_just_pressed("fishButtons"):
 		emit_signal("cast")
-	elif fishing  && Input.is_action_pressed("fishButtons"):
+	elif fishing && Input.is_action_pressed("fishButtons"):
 		apply_central_force(Vector2(0, up))
 	
